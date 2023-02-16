a99d9c77c62c79c8a013052b0a2198428892cd22
Daniel Stanziola <stanzioladaniel@gmail.com>
2023-02-15 21:34:00 -0300

    Added new features and improvements

--- src/main.js
+++ src/main.js
@@ -1,35 +1,76 @@
-getLocation();
+import conditions from '../conditions.js';
 
-function getLocation() {
+function getLocation(callback) {
   navigator.geolocation.getCurrentPosition(
-    (position) => callAPI(position.coords.latitude, position.coords.longitude),
-    (error) => alert(error.message)
+    position => {
+      const latAndLng =
+        position.coords.latitude + ',' + position.coords.longitude;
+      callback(latAndLng);
+    },
+    error => console.log(error)
   );
 }
 
-async function callAPI(latitude, longitude) {
+async function getInfoAPI(location) {
   try {
-    const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=936da3603d054ceabf414237230402&q=${latitude},${longitude}&aqi=no`);
+    const response = await fetch(
+      `http://api.weatherapi.com/v1/forecast.json?key=936da3603d054ceabf414237230402&q=${location}&aqi=no`
+    );
     const data = await response.json();
-    render(data);
     console.log(data);
+    render(data);
   } catch (error) {
-    alert(error.message);
+    error => console.log(error);
   }
 }
 
-function render(data) {
-  const day = data.forecast.forecastday[0];
-  document.querySelector('#city').innerHTML = `${data.location.name}, ${data.location.country}`;
-  document.querySelector('#icon-weather').src = `http:${data.current.condition.icon}`;
-  document.querySelector('#description').innerHTML = data.current.condition.text;
-  document.querySelector('#temp').innerHTML = `${data.current.temp_c}°c`;
-  document.querySelector('#temp_min').innerHTML = `${day.day.mintemp_c}°`;
-  document.querySelector('#temp_max').innerHTML = `${day.day.maxtemp_c}°`;
-  document.querySelector('#feelslike').innerHTML = `${data.current.feelslike_c}°`;
-  document.querySelector('#humidity').innerHTML = `${data.current.humidity}%`;
-  document.querySelector('#UV').innerHTML = data.current.uv;
-  document.querySelector('#wind').innerHTML = `${data.current.wind_dir} ${data.current.wind_kph}Km/h`;
-  document.querySelector('#visibility').innerHTML = `${data.current.vis_km} Km`;
-  document.querySelector('#update').innerHTML = `Actualizado a las ${data.current.last_updated.slice(11)}`;
+function render({ location, current, forecast }) {
+  document.querySelector('#img_weather').src = isDay(current.is_day);
+  document.querySelector(
+    '#city'
+  ).innerHTML = `${location.name}, ${location.country}`;
+  document.querySelector(
+    '#icon-weather'
+  ).src = `http:${current.condition.icon}`;
+  document.querySelector('#description').innerHTML = toSpanish(
+    current.condition,
+    conditions,
+    current.is_day
+  );
+  document.querySelector('#temp').innerHTML = `${current.temp_c}°c`;
+  document.querySelector(
+    '#temp_min'
+  ).innerHTML = `${forecast.forecastday[0].day.mintemp_c}°`;
+  document.querySelector(
+    '#temp_max'
+  ).innerHTML = `${forecast.forecastday[0].day.maxtemp_c}°`;
+  document.querySelector('#feelslike').innerHTML = `${current.feelslike_c}°`;
+  document.querySelector('#humidity').innerHTML = `${current.humidity}%`;
+  document.querySelector('#UV').innerHTML = current.uv;
+  document.querySelector(
+    '#wind'
+  ).innerHTML = `${current.wind_dir} ${current.wind_kph}Km/h`;
+  document.querySelector('#visibility').innerHTML = `${current.vis_km} Km`;
+  document.querySelector(
+    '#update'
+  ).innerHTML = `Actualizado a las ${current.last_updated.slice(11)}`;
 }
+
+function isDay(isDay) {
+  if (!isDay) return '../img/night.jpg';
+  return '../img/day.jpg';
+}
+
+function toSpanish(data, template, isDay) {
+  const code = template.filter(element => element.code == data.code);
+  const text = code[0].languages.filter(
+    element => element.lang_name == 'Spanish'
+  );
+
+  return isDay ? text[0].day_text : text[0].night_text;
+}
+
+window.onload = () => {
+  getLocation(getInfoAPI);
+  // getInfoAPI('-33.582788872337595, -70.9070157759715');
+};
